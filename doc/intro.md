38b4457bbd5eb1c9342a7f46537d4e2ad0a6aef8
infinitelytight <6633057+infinitelytight@users.noreply.github.com>
2019-03-29 07:44:25 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to feigenbaum
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/feigenbaum/core_test.clj
@@ -0,0 +1,7 @@
+(ns feigenbaum.core-test
+  (:require [clojure.test :refer :all]
+            [feigenbaum.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
