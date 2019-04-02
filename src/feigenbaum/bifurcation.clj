fab0d546353341f49efbf73cce18c1230865152a
infinitelytight <6633057+infinitelytight@users.noreply.github.com>
2019-04-02 17:56:56 +0100

    A graph shows

--- src/feigenbaum/bifurcation.clj
+++ src/feigenbaum/bifurcation.clj
@@ -1,9 +1,9 @@
-(ns feigenbaum.core)
+(ns feigenbaum.bifurcation)
 
 (defn round
   "Round to specified s.f."
   [d precision]
-  (let [factor (Math/pow 10 precision)]
+  (let [factor (Math/pow 10. precision)]
     (/ (Math/floor (* d factor)) factor)))
 
 (defn get-next-pop [previous lambda]
@@ -16,7 +16,7 @@
   (let [next-pop (get-next-pop current-pop lambda)]
     (if (> gens 0)
       (gen-pops (conj pops-list next-pop) next-pop lambda (- gens 1))
-      pops-list)))
+      (reverse pops-list))))
 
 (defn find-stable-pops
   "Finds the stable numbers between which the population cycles"
@@ -28,20 +28,21 @@
   [stable-pops-arr start-pop lambda gens step]
   (let [pops-list (gen-pops (list start-pop) start-pop lambda gens)
         stable-pops (find-stable-pops pops-list)]
-    (if (< lambda 4)
-      (gen-stable-pops (conj stable-pops-arr stable-pops) start-pop (+ lambda step) (- gens 1) step)
+    (if (< lambda 4.)
+      (gen-stable-pops
+        (conj stable-pops-arr stable-pops)
+        start-pop (+ lambda step) (- gens 1) step)
       stable-pops-arr)))
 
-(defn -main
-  [& args]
-  (let [start-pop 0.5 ; between 0 and 1
-        lambda 0 ; between 0 and 4
-        gens 250 ; might need to be higher
-        step 0.1 ; increase granularity when inspecting 3.5 - 4 range
-        ]
-  (def stable-pops (gen-stable-pops (vector) start-pop lambda gens step)))
-  (for [p stable-pops]
-    (println p)))
-
-(-main)
+;(defn -main
+;  [& args]
+;  (let [start-pop 0.5 ; between 0 and 1
+;        lambda 0 ; between 0 and 4
+;        gens 250 ; might need to be higher
+;        step 0.1 ; increase granularity when inspecting 3.5 - 4 range
+;        ]
+;  (def stable-pops (gen-stable-pops (vector) start-pop lambda gens step)))
+;  stable-pops)
+;
+;(-main)
 
--- src/feigenbaum/core.clj
+++ src/feigenbaum/core.clj
@@ -1,25 +1,41 @@
 (ns feigenbaum.core
   (:gen-class)
-  (:use [compojure.core])
+  (:use [compojure.core]
+        [feigenbaum.bifurcation])
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.route :as route]
             [ring.util.response :as res]))
 
-(defn bifurcations
-  "Generates bifurcation data"
+(def start-pop 0.5)
+
+(defn populations
+  "Generates population figures"
   [request]
-  (let [start       (Integer. (get-in request [:route-params :start]))
-        lambda      (Integer. (get-in request [:route-params :generations]))
-        generations (Integer. (get-in request [:route-params :generations]))
+  (let [lambda (Double.  (get-in request [:route-params :lambda]))
+        gens   (Integer. (get-in request [:route-params :generations]))
         err (= 0 1)]
     (if err
-      {:status 500, :body "Error"}
-      {:status 200, :body (str start lambda generations) })))
+      {:status 500,
+       :body "Error"}
+      {:status 200,
+       :body (str (gen-pops (list start-pop) start-pop lambda gens))})))
+
+;(defn bifurcations
+;  "Generates bifurcation data"
+;  [request]
+;  (let [start       (Integer. (get-in request [:route-params :start]))
+;        lambda      (Integer. (get-in request [:route-params :generations]))
+;        generations (Integer. (get-in request [:route-params :generations]))
+;        err (= 0 1)]
+;    (if err
+;      {:status 500, :body "Error"}
+;      {:status 200, :body (str start lambda generations) })))
 
 (defroutes app
   (GET "/" [] (res/file-response "index.html" {:root "resources/public"}))
-  (GET "/bifurcations/:start/:lambda/:generations" [] bifurcations)
+  (GET "/populations/:lambda/:generations" [] populations)
+;  (GET "/bifurcations/:start/:lambda/:generations" [] bifurcations)
   (route/resources "/")
   (route/not-found "<h1>404</h1>"))
 
