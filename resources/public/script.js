fb596808a77cb3ba02b2b795e3418771d5399b37
infinitelytight <6633057+infinitelytight@users.noreply.github.com>
2019-04-05 12:14:12 +0100

    Graph updates on data change

--- resources/public/script.js
+++ resources/public/script.js
@@ -4,14 +4,65 @@ $(document).ready(function(){
 
 })
 
-$("#generate").click(function() {
-  let lambda = $("#lambda").val();
-  let generations = $("#generations").val();
+data = []
+
+let margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+}
+
+let width = 960 - margin.left - margin.right
+let height = 500 - margin.top - margin.bottom
+
+let x = d3.scaleBand().range([0, width])
+let y = d3.scaleLinear().range([height, 0])
+
+y.domain([0, 1])
+
+let line = d3.line()
+
+let svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+svg.append("path")
+    .data([data])
+    .attr("class", "line")
+    .attr("d", line)
+
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+
+svg.append("g")
+    .call(d3.axisLeft(y))
+
+function updateGraph(lambda, generations) {
+  fetchData(lambda, generations, function(data) {
+    x.domain(data.map(function(d) { return d.generation }))
+
+    let line = d3.line()
+      .x(function(d) { return x(d.generation) })
+      .y(function(d) { return y(d.population) })
+
+    var svg = d3.select("svg").transition()
+
+    svg.select(".line")
+        .duration(750)
+        .attr("d", line(data))
+  })
+}
+
+function fetchData(lambda, generations, callback) {
   $.get("/populations/" + lambda + "/" + generations, function(res) {
     let data = parseResponse(res)
-    graphPopulations(data)
+    callback(data)
   })
-})
+}
 
 function parseResponse(res) {
   data = res.slice(1,-1).split(" ")
@@ -21,43 +72,16 @@ function parseResponse(res) {
   return data
 }
 
-function graphPopulations(data) {
-  let margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
-  }
+$("#generate").click(function() {
+  let lambda = $("#lambda").val()
+  let generations = $("#generations").val()
 
-  let width = 960 - margin.left - margin.right
-  let height = 500 - margin.top - margin.bottom
+  updateGraph(lambda, generations)
+})
 
-  let x = d3.scaleBand().range([0, width])
-  let y = d3.scaleLinear().range([height, 0])
+$('input[type=range]').change(function(){
+  let lambda = $(this).val()
+  $("#lambda_value").text(lambda)
 
-  x.domain(data.map(function(d) { return d.generation }))
-  y.domain([0, d3.max(data, function(d) { return d.population })])
-
-  let line = d3.line()
-    .x(function(d) { return x(d.generation); })
-    .y(function(d) { return y(d.population); })
-
-  let svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", line);
-
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-  svg.append("g")
-      .call(d3.axisLeft(y));
-}
-
+  updateGraph(lambda, 100)
+});
