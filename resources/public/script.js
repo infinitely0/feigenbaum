ad6c22e8a84e5621db366b025c4f946ff85fdca5
infinitelytight <6633057+infinitelytight@users.noreply.github.com>
2019-04-05 08:52:13 +0100

    Population data graphed

--- resources/public/script.js
+++ resources/public/script.js
@@ -7,25 +7,57 @@ $(document).ready(function(){
 $("#generate").click(function() {
   let lambda = $("#lambda").val();
   let generations = $("#generations").val();
-  $.get("/populations/" + lambda + "/" + generations, function(data) {
-    graphPopulation(data)
+  $.get("/populations/" + lambda + "/" + generations, function(res) {
+    let data = parseResponse(res)
+    graphPopulations(data)
   })
 })
 
-function graphPopulation(data) {
-  var data = parseResponse(data)
-  var x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
-
-  d3.select(".chart")
-    .selectAll("div")
-      .data(data)
-    .enter().append("div")
-      .style("width", function(d) { return x(d) + "px"; })
-      .text(function(d) { return d; });
+function parseResponse(res) {
+  data = res.slice(1,-1).split(" ")
+  data.forEach(function(val, i) {
+    data[i] = { generation: i, population: val }
+  })
+  return data
 }
 
-function parseResponse(data) {
-  return data.slice(1,-1).split(" ")
+function graphPopulations(data) {
+  let margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+  }
+
+  let width = 960 - margin.left - margin.right
+  let height = 500 - margin.top - margin.bottom
+
+  let x = d3.scaleBand().range([0, width])
+  let y = d3.scaleLinear().range([height, 0])
+
+  x.domain(data.map(function(d) { return d.generation }))
+  y.domain([0, d3.max(data, function(d) { return d.population })])
+
+  let line = d3.line()
+    .x(function(d) { return x(d.generation); })
+    .y(function(d) { return y(d.population); })
+
+  let svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", line);
+
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  svg.append("g")
+      .call(d3.axisLeft(y));
 }
+
